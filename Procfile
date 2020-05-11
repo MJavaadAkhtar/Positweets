1d55ac7e521d49820a5b0bb6ca8e1d85a3244dce
MJavaadAkhtar <javaad.akhtar@mail.utoronto.ca>
2020-05-11 00:14:44 -0400

    new file added

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn positweet.positweet.wsgi
--- positweet/positweet/settings.py
+++ positweet/positweet/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +21,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c2n*&-jie)n+_12z2bc39=jy!b75jzl2opg_^c85&hdkc)&pre'
+# SECRET_KEY = 'c2n*&-jie)n+_12z2bc39=jy!b75jzl2opg_^c85&hdkc)&pre'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['posi-tweets.herokuapp.com']
 
 
 # Application definition
@@ -122,5 +124,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+
+django_heroku.settings(locals())
